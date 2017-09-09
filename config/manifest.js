e744aafb456d15cc9f527d3f08a9250981b2c56e
Greg Hurlman <greg@greghurlman.com>
2017-09-08 22:45:13 -0700

    manifest changes

---
+++ config/manifest.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "scc-pwa",
+    short_name: "scc-pwa",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/ember-welcome-page/images/construction.png",
+        sizes: "540x540",
+        type: "image/png"
+      }
+    ]
+  };
+}
